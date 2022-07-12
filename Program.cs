e0124f272805a8fe7c96f70e0eb9085c61dd71ed
juholiver <juholiver3@gmail.com>
2022-07-12 09:27:33 -0300

    Menu Opções Usuarios

--- Program.cs
+++ Program.cs
@@ -11,6 +11,25 @@ namespace Dio.Bank
         System.Console.WriteLine(minhaConta.ToString());
         
     }
+    private static string ObterOpcaoUsuario()
+    {
+        Console.WriteLine();
+        Console.WriteLine("DIO Bank a seu dispor!!!");
+        Console.WriteLine("Informe a opção desejada: ");
+
+        Console.WriteLine("1- Listar contas");
+        Console.WriteLine("2- Inserir nova conta ");
+        Console.WriteLine("3- tranferir");
+        Console.WriteLine("4- Sacar");
+        Console.WriteLine("5- Depositar");
+        Console.WriteLine("C- Limpar tela ");
+        Console.WriteLine("X- Sair");
+        Console.WriteLine();
+
+        string opcaoUsuario = Console.ReadLine().ToUpper();
+        Console.WriteLine();
+        return opcaoUsuario;
+    }
   }
     
 }
