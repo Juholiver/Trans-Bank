1b4e865369ccc2d6d6948c4b42f4ec14e077bad5
juholiver <juholiver3@gmail.com>
2022-07-12 08:04:20 -0300

    Adicionando Contrutor Tipo Conta

--- Classes/Conta.cs
+++ Classes/Conta.cs
@@ -5,6 +5,14 @@ namespace Dio.Bank
        private TipoConta TipoConta { get; set; }
        private double Saldo { get; set; }
        private double Cretido { get; set; }
-       private string Nome { get; set; } 
+       private string Nome { get; set; }
+
+       public Conta(TipoConta tipoConta, double saldo, double credito, string nome)
+       {
+        this.TipoConta = tipoConta;
+        this.Saldo = saldo;
+        this.Cretido = credito;
+        this.Nome = nome;
+       } 
     }
 }
