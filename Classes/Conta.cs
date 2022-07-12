52f79ffb65bf77a5f472925a0e95bdb619a68322
juholiver <juholiver3@gmail.com>
2022-07-12 08:01:07 -0300

    Criando Classe Conta e enum tipo Conta

---
+++ Classes/Conta.cs
@@ -0,0 +1,10 @@
+namespace Dio.Bank
+{
+    public class Conta
+    {
+       private TipoConta TipoConta { get; set; }
+       private double Saldo { get; set; }
+       private double Cretido { get; set; }
+       private string Nome { get; set; } 
+    }
+}
